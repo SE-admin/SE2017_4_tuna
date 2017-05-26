31338f0c2ac4e2c673487588254a118135398a39
201311109 <sodeinjumin@gmail.com>
2017-05-27 00:07:43 +0900

    #751 src/main/java/se/smu/MainFrame.java // 과목확인을 위한 Frame 생성

--- src/main/java/se/smu/MainFrame.java
+++ src/main/java/se/smu/MainFrame.java
@@ -1,10 +1,22 @@
 package se.smu;
 
-public class MainFrame {
+import javax.swing.JFrame;
 
+public class MainFrame extends JFrame {
+
+	MainFrame(){
+		//초기화면 설정
+		setTitle("과목/ToDo 관리");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLayout(null);
+		setResizable(false);
+		setSize(1200, 700);
+		setVisible(true);
+		//초기화면 설정
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("HI");
+		new MainFrame();
 	}
 
 }
